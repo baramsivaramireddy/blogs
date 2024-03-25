1a88423019a4e5255717da34f5d8989b83ddd5fb
baramsivaramireddy <baramsivarami@gmail.com>
2024-03-25 09:37:43 +0530

    added patch blog

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,25 +2,25 @@ const jwt = require("jsonwebtoken");
 module.exports = {
   authenticationMiddleware: async function (req, res, next) {
     try {
-      if (__configurations.ENV_TYPE=='local') {
+      if (__configurations.ENVIRONMENT == 'local') {
         next();
         return "";
       }
       let token = req?.headers?.authorization?.split(" ")[1];
 
-      console.log(token)
+
       if (token === undefined) {
         res.status(401).json({ message: " authorization  token is required" });
         return "";
       }
 
-    
+
       try {
         let decoded = jwt.verify(token, __configurations.SECRETKEY);
         req.user = decoded;
       } catch (err) {
-        
-        res.status(401).json({ message: "  token is invalid"+err });
+
+        res.status(401).json({ message: "  token is invalid" + err });
         return "";
       }
       next();
@@ -32,19 +32,18 @@ module.exports = {
   },
   authorizationMiddleware: function (allowedRoleArray) {
     return function (req, res, next) {
-  
-      try {
 
-        if (__configurations.ENV_TYPE=='local') {
-            next();
-            return "";
-          }
-            // Need to change
-          let isAllowed = allowedRoleArray.includes(req.user.role)
-          if (!isAllowed){
-            res.status(403).json({message:"not permitted"})
-            return
-          }
+      try {
+        if (__configurations.ENVIRONMENT == 'local') {
+          next();
+          return "";
+        }
+        // Need to change
+        let isAllowed = allowedRoleArray.includes(req.user.role)
+        if (!isAllowed) {
+          res.status(403).json({ message: "not permitted" })
+          return
+        }
         next();
       } catch (err) {
         console.log(`Error while authorizing the token ${err}`);
